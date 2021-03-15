bdabc7cab0730b1fcd889f39b13cd6d715fc6126
Ivanluca <jungjihoonkr97@gmail.com>
2021-03-15 13:12:40 +0700

    [Code create & config] emoji assets/js (#13)

---
+++ coffee-client/src/assets/js/emoji.js
@@ -0,0 +1,172 @@
+export default {
+  "[smile]":
+    "//i0.hdslb.com/bfs/emote/685612eadc33f6bc233776c6241813385844f182.png@112w_112h.webp",
+
+  "[laugh]":
+    "//i0.hdslb.com/bfs/emote/81edf17314cea3b48674312b4364df44d5c01f17.png@112w_112h.webp",
+
+  "[babble]":
+    "//i0.hdslb.com/bfs/emote/b5a5898491944a4268360f2e7a84623149672eb6.png@112w_112h.webp",
+
+  "[OK]":
+    "//i0.hdslb.com/bfs/emote/4683fd9ffc925fa6423110979d7dcac5eda297f4.png@112w_112h.webp",
+
+  "[Star eye]":
+    "//i0.hdslb.com/bfs/emote/63c9d1a31c0da745b61cdb35e0ecb28635675db2.png@112w_112h.webp",
+
+  "[Oh hoo]":
+    "//i0.hdslb.com/bfs/emote/362bded07ea5434886271d23fa25f5d85d8af06c.png@112w_112h.webp",
+
+  "[dislike]":
+    "//i0.hdslb.com/bfs/emote/de4c0783aaa60ec03de0a2b90858927bfad7154b.png@112w_112h.webp",
+
+  "[like]":
+    "//i0.hdslb.com/bfs/emote/8a10a4d73a89f665feff3d46ca56e83dc68f9eb8.png@112w_112h.webp",
+
+  "[Sour]":
+    "//i0.hdslb.com/bfs/emote/92b1c8cbceea3ae0e8e32253ea414783e8ba7806.png@112w_112h.webp",
+
+  "[Cry]":
+    "//i0.hdslb.com/bfs/emote/2caafee2e5db4db72104650d87810cc2c123fc86.png@112w_112h.webp",
+
+  "[shy]":
+    "//i0.hdslb.com/bfs/emote/9d2ec4e1fbd6cb1b4d12d2bbbdd124ccb83ddfda.png@112w_112h.webp",
+
+  "[Speechless]":
+    "//i0.hdslb.com/bfs/emote/44667b7d9349957e903b1b62cb91fb9b13720f04.png@112w_112h.webp",
+
+  "[doubt]":
+    "//i0.hdslb.com/bfs/emote/b7840db4b1f9f4726b7cb23c0972720c1698d661.png@112w_112h.webp",
+
+  "[naughty]":
+    "//i0.hdslb.com/bfs/emote/8290b7308325e3179d2154327c85640af1528617.png@112w_112h.webp",
+
+  "[Laugh and cry]":
+    "//i0.hdslb.com/bfs/emote/485a7e0c01c2d70707daae53bee4a9e2e31ef1ed.png@112w_112h.webp",
+
+  "[sinister smile]":
+    "//i0.hdslb.com/bfs/emote/bb84906573472f0a84cebad1e9000eb6164a6f5a.png@112w_112h.webp",
+
+  "[Snicker]":
+    "//i0.hdslb.com/bfs/emote/6c49d226e76c42cd8002abc47b3112bc5a92f66a.png@112w_112h.webp",
+
+  "[laughing out loud]":
+    "//i0.hdslb.com/bfs/emote/ca94ad1c7e6dac895eb5b33b7836b634c614d1c0.png@112w_112h.webp",
+
+  "[insidious]":
+    "//i0.hdslb.com/bfs/emote/ba8d5f8e7d136d59aab52c40fd3b8a43419eb03c.png@112w_112h.webp",
+
+  "[Cover face]":
+    "//i0.hdslb.com/bfs/emote/6921bb43f0c634870b92f4a8ad41dada94a5296d.png@112w_112h.webp",
+
+  "[stay]":
+    "//i0.hdslb.com/bfs/emote/33ad6000d9f9f168a0976bc60937786f239e5d8c.png@112w_112h.webp",
+
+  "[Pick your nose]":
+    "//i0.hdslb.com/bfs/emote/cb89184c97e3f6d50acfd7961c313ce50360d70f.png@112w_112h.webp",
+
+  "[Surprise]":
+    "//i0.hdslb.com/bfs/emote/0afecaf3a3499479af946f29749e1a6c285b6f65.png@112w_112h.webp",
+
+  "[Surprised]":
+    "//i0.hdslb.com/bfs/emote/f8e9a59cad52ae1a19622805696a35f0a0d853f3.png@112w_112h.webp",
+
+  "[Cover your face and cry]":
+    "//i0.hdslb.com/bfs/emote/c3043ba94babf824dea03ce500d0e73763bf4f40.png@112w_112h.webp",
+
+  "[Wonderful]":
+    "//i0.hdslb.com/bfs/emote/b4cb77159d58614a9b787b91b1cd22a81f383535.png@112w_112h.webp",
+
+  "[Dog head]":
+    "//i0.hdslb.com/bfs/emote/bba7c12aa51fed0199c241465560dfc2714c593e.png@112w_112h.webp",
+
+  "[funny]":
+    "//i0.hdslb.com/bfs/emote/d15121545a99ac46774f1f4465b895fe2d1411c3.png@112w_112h.webp",
+
+  "[Eat melon]":
+    "//i0.hdslb.com/bfs/emote/4191ce3c44c2b3df8fd97c33f85d3ab15f4f3c84.png@112w_112h.webp",
+
+  "[Call]":
+    "//i0.hdslb.com/bfs/emote/431432c43da3ee5aab5b0e4f8931953e649e9975.png@112w_112h.webp",
+
+  "[likes]":
+    "//i0.hdslb.com/bfs/emote/1a67265993913f4c35d15a6028a30724e83e7d35.png@112w_112h.webp",
+
+  "[applaud]":
+    "//i0.hdslb.com/bfs/emote/895d1fc616b4b6c830cf96012880818c0e1de00d.png@112w_112h.webp",
+
+  "[awkward]":
+    "//i0.hdslb.com/bfs/emote/cb321684ed5ce6eacdc2699092ab8fe7679e4fda.png@112w_112h.webp",
+
+  "[cold]":
+    "//i0.hdslb.com/bfs/emote/cb0ebbd0668640f07ebfc0e03f7a18a8cd00b4ed.png@112w_112h.webp",
+
+  "[Soul out of the body]":
+    "//i0.hdslb.com/bfs/emote/43d3db7d97343c01b47e22cfabeca84b4251f35a.png@112w_112h.webp",
+
+  "[Wronged]":
+    "//i0.hdslb.com/bfs/emote/d2f26cbdd6c96960320af03f5514c5b524990840.png@112w_112h.webp",
+
+  "[Proud]":
+    "//i0.hdslb.com/bfs/emote/010540d0f61220a0db4922e4a679a1d8eca94f4e.png@112w_112h.webp",
+
+  "[pain]":
+    "//i0.hdslb.com/bfs/emote/905fd9a99ec316e353b9bd4ecd49a5f0a301eabf.png@112w_112h.webp",
+
+  "[scare]":
+    "//i0.hdslb.com/bfs/emote/9c10c5ebc7bef27ec641b8a1877674e0c65fea5d.png@112w_112h.webp",
+
+  "[sick]":
+    "//i0.hdslb.com/bfs/emote/0f25ce04ae1d7baf98650986454c634f6612cb76.png@112w_112h.webp",
+
+  "[threw up]":
+    "//i0.hdslb.com/bfs/emote/06946bfe71ac48a6078a0b662181bb5cad09decc.png@112w_112h.webp",
+
+  "[Boo]":
+    "//i0.hdslb.com/bfs/emote/e64af664d20716e090f10411496998095f62f844.png@112w_112h.webp",
+
+  "[Cover your eyes]":
+    "//i0.hdslb.com/bfs/emote/c5c6d6982e1e53e478daae554b239f2b227b172b.png@112w_112h.webp",
+
+  "[Thinking]":
+    "//i0.hdslb.com/bfs/emote/cfa9b7e89e4bfe04bbcd34ccb1b0df37f4fa905c.png@112w_112h.webp",
+
+  "[Goodbye]":
+    "//i0.hdslb.com/bfs/emote/fc510306bae26c9aec7e287cdf201ded27b065b9.png@112w_112h.webp",
+
+  "[Roll your eyes]":
+    "//i0.hdslb.com/bfs/emote/eba54707c7168925b18f6f8b1f48d532fe08c2b1.png@112w_112h.webp",
+
+  "[yawn]":
+    "//i0.hdslb.com/bfs/emote/888d877729cbec444ddbd1cf4c9af155a7a06086.png@112w_112h.webp",
+
+  "[struggle]":
+    "//i0.hdslb.com/bfs/emote/bb2060c15dba7d3fd731c35079d1617f1afe3376.png@112w_112h.webp",
+
+  "[sunglasses]":
+    "//i0.hdslb.com/bfs/emote/3a03aebfc06339d86a68c2d893303b46f4b85771.png@112w_112h.webp",
+
+  "[Squirming]":
+    "//i0.hdslb.com/bfs/emote/531863568e5668c5ac181d395508a0eeb1f0cda4.png@112w_112h.webp",
+
+  "[Sad]":
+    "//i0.hdslb.com/bfs/emote/a651db36701610aa70a781fa98c07c9789b11543.png@112w_112h.webp",
+
+  "[Crazy]":
+    "//i0.hdslb.com/bfs/emote/4c87afff88c22439c45b79e9d2035d21d5622eba.png@112w_112h.webp",
+
+  "[pissed off]":
+    "//i0.hdslb.com/bfs/emote/3195714219c4b582a4fb02033dd1519913d0246d.png@112w_112h.webp",
+
+  "[Love]":
+    "//i0.hdslb.com/bfs/emote/ed04066ea7124106d17ffcaf75600700e5442f5c.png@112w_112h.webp",
+
+  "[victory]":
+    "//i0.hdslb.com/bfs/emote/b49fa9f4b1e7c3477918153b82c60b114d87347c.png@112w_112h.webp",
+
+  "[bless]":
+    "//i0.hdslb.com/bfs/emote/fafe8d3de0dc139ebe995491d2dac458a865fb30.png@112w_112h.webp",
+
+  "[stand by]":
+    "//i0.hdslb.com/bfs/emote/3c210366a5585706c09d4c686a9d942b39feeb50.png@112w_112h.webp"
+};
