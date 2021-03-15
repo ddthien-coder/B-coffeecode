0fc7a1d3c9b158f92b6005f26a3ac038c791ac56
Ivanluca <jungjihoonkr97@gmail.com>
2021-03-15 13:27:32 +0700

    [Code update & config] Project (#19)

--- coffee-client/src/App.vue
+++ coffee-client/src/App.vue
@@ -1,32 +1,47 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+   <v-app id="app">
+     <!-- Navigation bar -->
+     <TopNavBar></TopNavBar>
+     <!-- Side navigation bar -->
+     <SideNavBar></SideNavBar>
+     <!-- Content -->
+     <v-content>
+       <router-view :key="$route.fullPath"></router-view>
+     </v-content>
+     <!-- Footer -->
+     <Footer></Footer>
+     <!-- Back to top -->
+     <BackTop></BackTop>
+     <!-- Search modal box -->
+     <searchModel></searchModel>
+     <!-- Login modal box -->
+     <LoginModel></LoginModel>
+     <!-- Register modal box -->
+     <RegisterModel></RegisterModel>
+     <!-- Forgot password modal box -->
+     <ForgetModel></ForgetModel>
+   </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import TopNavBar from "./components/layout/TopNavBar";
+import SideNavBar from "./components/layout/SideNavBar";
+import Footer from "./components/layout/Footer";
+import BackTop from "./components/BackTop";
+import searchModel from "./components/model/SearchModel";
+import LoginModel from "./components/model/LoginModel";
+import RegisterModel from "./components/model/RegisterModel";
+import ForgetModel from "./components/model/ForgetModel";
+export default {
+  components: {
+    TopNavBar,
+    SideNavBar,
+    Footer,
+    BackTop,
+    searchModel,
+    LoginModel,
+    RegisterModel,
+    ForgetModel
   }
-}
-</style>
+};
+</script>
